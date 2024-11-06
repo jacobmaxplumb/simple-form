1c1cfb7831e647720545ca7eebe276eddebadeb6
Jacob Plumb <jacobmaxplumb@gmail.com>
2024-11-06 18:55:41 -0500

    simple form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+const initialFormState = {
+  fullName: '',
+  nickName: '',
+  shirtSize: '',
+  inAgreement: false,
+  isChecked: false
+}
 
 function App() {
+  const [formValues, setFormValues] = useState(initialFormState);
+
+  const handleChange = (e) => {
+    const {value, checked, id, type} = e.target;
+    if (type === 'checkbox') {
+      setFormValues({...formValues, [id]: checked})
+    } else {
+      setFormValues({...formValues, [id]: value});
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form>
+      <input value={formValues.fullName} id="fullName" onChange={handleChange} />
+      <input value={formValues.nickName} id="nickName" onChange={handleChange} />
+      <select value={formValues.shirtSize} id="shirtSize" onChange={handleChange}>
+        <option value=""></option>
+        <option value="S">Small</option>
+        <option value="M">Medium</option>
+        <option value="L">Large</option>
+      </select>
+      <input checked={formValues.inAgreement} id="inAgreement" type="checkbox" onChange={handleChange} /> I Agree
+      <input checked={formValues.isChecked} id="isChecked" type="checkbox" onChange={handleChange} /> I Agree
+    </form>
   );
 }
 
